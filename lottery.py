454b68ca9ce7aefa14e9bfcfd4bc1e03f4c4880d
Irish Khristine M. Del Rosario <92616151+irishhhhh@users.noreply.github.com>
2022-01-12 18:57:13 +0800

    Update lottery.py

--- lottery.py
+++ lottery.py
@@ -2,7 +2,7 @@ def lottery():
 
     import random
 
-    n1 = int(input("Enter 1st number: "))
+    n1 = input("Enter 1st number: ")
     n2 = input("Enter 2nd number: ")
     n3 = input("Enter 3rd number: ")
 
